1cb72ec21fb15d2c3275cdcb9b37d74a1c774808
Alain <alain.holzhauser@mmp.htwchur.ch>
2019-10-29 15:46:28 +0100

    backend 3

--- router/RouterWebShop.js
+++ router/RouterWebShop.js
@@ -23,91 +23,91 @@ export class RouterWebShop {
         this.controllerOrder = new ControllerOrder(storeShoppingBasket, storeSession, storeUser);
 
         // GET
-        this.router.get('/backend//article', async (request, response) => {
+        this.router.get('/backend/article', async (request, response) => {
             await this.controllerArticle.getArticle(request, response);
         });
-        this.router.get('/backend//articles', async (request, response) => {
+        this.router.get('/backend/articles', async (request, response) => {
             await this.controllerArticle.getArticles(request, response);
         });
-        this.router.get('/backend//articles/latest', async (request, response) => {
+        this.router.get('/backend/articles/latest', async (request, response) => {
             await this.controllerArticle.getArticlesLatest(request, response);
         });
-        this.router.get('/backend//user', async (request, response) => {
+        this.router.get('/backend/user', async (request, response) => {
             await this.controllerUser.getUser(request, response);
         });
-        this.router.get('/backend//users', authentication.required(), async (request, response) => {
+        this.router.get('/backend/users', authentication.required(), async (request, response) => {
             await this.controllerUser.getUsers(request, response);
         });
-        this.router.get('/backend//shopping-basket', async (request, response) => {
+        this.router.get('/backend/shopping-basket', async (request, response) => {
             await this.controllerShoppingBasket.get(request, response);
         });
-        this.router.get('/backend//order', authentication.required(), async (request, response) => {
+        this.router.get('/backend/order', authentication.required(), async (request, response) => {
             await this.controllerOrder.get(request, response);
         });
-        this.router.get('/backend//orders', authentication.required(), async (request, response) => {
+        this.router.get('/backend/orders', authentication.required(), async (request, response) => {
             await this.controllerOrder.getOrders(request, response);
         });
-        this.router.get('/backend//orders/user', authentication.required(), async (request, response) => {
+        this.router.get('/backend/orders/user', authentication.required(), async (request, response) => {
             await this.controllerOrder.getOrdersByUser(request, response);
         });
 
         // POST
-        this.router.post('/backend//sign-in/user', async (request, response) => {
+        this.router.post('/backend/sign-in/user', async (request, response) => {
             await this.controllerUser.signIn(request, response);
         });
-        this.router.post('/backend//sign-out/user', async (request, response) => {
+        this.router.post('/backend/sign-out/user', async (request, response) => {
             await this.controllerUser.signOut(request, response);
         });
-        this.router.post('/backend//create/user', async (request, response) => {
+        this.router.post('/backend/create/user', async (request, response) => {
             await this.controllerUser.create(request, response);
         });
         this.router.post('/backend/create/shopping-basket', async (request, response) => {
             await this.controllerShoppingBasket.create(request, response);
         });
-        this.router.post('/backend//create/order', authentication.required(), async (request, response) => {
+        this.router.post('/backend/create/order', authentication.required(), async (request, response) => {
             await this.controllerOrder.create(request, response);
         });
-        this.router.post('/backend//delete/user', async (request, response) => {
+        this.router.post('/backend/delete/user', async (request, response) => {
             await this.controllerUser.deleteUser(request, response);
         });
-        this.router.patch('/backend//delete/order', async (request, response) => {
+        this.router.patch('/backend/delete/order', async (request, response) => {
             await this.controllerOrder.deleteOrder(request, response);
         });
-        this.router.post('/backend//add-item/shopping-basket', async (request, response) => {
+        this.router.post('/backend/add-item/shopping-basket', async (request, response) => {
             await this.controllerShoppingBasket.addItem(request, response);
         });
-        this.router.post('/backend//remove-item/shopping-basket', async (request, response) => {
+        this.router.post('/backend/remove-item/shopping-basket', async (request, response) => {
             await this.controllerShoppingBasket.removeItem(request, response);
         });
 
         // PUT
-        this.router.put('/backend//update/user', async (request, response) => {
+        this.router.put('/backend/update/user', async (request, response) => {
             await this.controllerUser.updateUser(request, response);
         });
-        this.router.put('/backend//update/order', async (request, response) => {
+        this.router.put('/backend/update/order', async (request, response) => {
             await this.controllerOrder.updateOrder(request, response);
         });
-        this.router.put('/backend//update/order/delivery-address', authentication.required(), async (request, response) => {
+        this.router.put('/backend/update/order/delivery-address', authentication.required(), async (request, response) => {
             await this.controllerOrder.change(request, response);
         });
-        this.router.put('/backend//update/order/contact-data', authentication.required(), async (request, response) => {
+        this.router.put('/backend/update/order/contact-data', authentication.required(), async (request, response) => {
             await this.controllerOrder.change(request, response);
         });
-        this.router.put('/backend//update/order/delivery-type', authentication.required(), async (request, response) => {
+        this.router.put('/backend/update/order/delivery-type', authentication.required(), async (request, response) => {
             await this.controllerOrder.change(request, response);
         });
-        this.router.put('/backend//update/order/payment-type', authentication.required(), async (request, response) => {
+        this.router.put('/backend/update/order/payment-type', authentication.required(), async (request, response) => {
             await this.controllerOrder.change(request, response);
         });
 
         // PATCH
-        this.router.patch('/backend//update/order/state', authentication.required(), authentication.required(), async (request, response) => {
+        this.router.patch('/backend/update/order/state', authentication.required(), authentication.required(), async (request, response) => {
             await this.controllerOrder.changeState(request, response);
         });
-        this.router.patch('/backend//change-rating/article', async (request, response) => {
+        this.router.patch('/backend/change-rating/article', async (request, response) => {
             await this.controllerArticle.changeArticleRating(request, response);
         });
-        this.router.patch('/backend//change-item-amount/shopping-basket', async (request, response) => {
+        this.router.patch('/backend/change-item-amount/shopping-basket', async (request, response) => {
             await this.controllerShoppingBasket.changeItemAmount(request, response);
         });
     }
